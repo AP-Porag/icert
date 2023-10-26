2837e1dbbdab407616692bca0b87530968f7b88f
AP-Porag <smauhp@gmail.com>
2023-10-26 18:49:28 +0600

    command route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\Profile\UserProfileController;
 use App\Http\Controllers\Admin\User\UsersController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('login');
+});
+
+Route::get('/execute-command', function () {
+//    return redirect()->route('login');
+    Artisan::call('storage:link');
+    dd('All commands executed successfully');
 });
 
 Auth::routes();
