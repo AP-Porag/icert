8d4feda33249a2b361bc7a59e2d36d7f88d51d2a
AP-Porag <smauhp@gmail.com>
2023-10-30 01:04:28 +0600

    ksa off

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -43,19 +43,19 @@ class UserSeeder extends Seeder
                 'phone'             => '012345678910',
                 'date_of_hire'     => '2000-10-21',
             ],
-            [
-                'first_name'        => 'Ksa',
-                'last_name'         => 'Last',
-                'username'         => 'ksalast',
-                'email'             => 'ksa@app.com',
-                'email_verified_at' => now(),
-                'password'          => Hash::make("12345678"),   // 12345678
-                'user_type'         => User::USER_TYPE_KSA,
-                'status'            => GlobalConstant::STATUS_ACTIVE,
-                'remember_token'    => Str::random(60),
-                'phone'             => '012345678910',
-                'date_of_hire'     => '1995-05-15',
-            ],
+//            [
+//                'first_name'        => 'Ksa',
+//                'last_name'         => 'Last',
+//                'username'         => 'ksalast',
+//                'email'             => 'ksa@app.com',
+//                'email_verified_at' => now(),
+//                'password'          => Hash::make("12345678"),   // 12345678
+//                'user_type'         => User::USER_TYPE_KSA,
+//                'status'            => GlobalConstant::STATUS_ACTIVE,
+//                'remember_token'    => Str::random(60),
+//                'phone'             => '012345678910',
+//                'date_of_hire'     => '1995-05-15',
+//            ],
         ];
 
 
